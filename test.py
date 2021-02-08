1bb1b9a858f73504f085fec693c0475908381744
monta0315 <monta0315@docomo.ne.jp>
2021-02-08 22:16:31 +0900

    environment_variable_num is changed

--- test.py
+++ test.py
@@ -14,8 +14,8 @@ import os
 app = Flask(__name__)
 
 #環境変数取得
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ["AtNoM1999"]
-YOUR_CHANNEL_SECRET = os.environ["AtNoM1999"]
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
