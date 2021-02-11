498fea7cf12df7435c702d03300cdcc0070bccd3
monta0315 <monta0315@docomo.ne.jp>
2021-02-11 16:00:06 +0900

    b

--- test.py
+++ test.py
@@ -52,5 +52,5 @@ def handle_message(event):
 
 if __name__ == "__main__":
     #    app.run()
-    port = int(os.getenv("PORT", 5000))
+    port = int(os.getenv("PORT"))
     app.run(host="0.0.0.0", port=port)
