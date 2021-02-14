8f317715146b02ff1be5d9aa752deccad668a6f8
monta0315 <monta0315@docomo.ne.jp>
2021-02-14 12:44:38 +0900

    a

--- test.py
+++ test.py
@@ -74,7 +74,7 @@ def handle_message(event):
 
 
     #flex_boxに変換
-    msg = FlexSendMessage(
+    flex_message = FlexSendMessage(
         alt_text="hello",
         contents=msg_create())
 
@@ -82,7 +82,7 @@ def handle_message(event):
 
 
     #メッセージを送信するフェーズ
-    line_bot_api.reply_message(event.reply_token,msg)
+    line_bot_api.reply_message(event.reply_token,flex_message)
 
 
 def youtube(push_text):
@@ -95,157 +95,19 @@ def youtube(push_text):
     return search_response
 
 def msg_create():
-    msg = {
-        "type": "bubble",
-        "hero": {
-            "type": "image",
-            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-            "size": "full",
-            "aspectRatio": "20:13",
-            "aspectMode": "cover",
-            "action": {
-                "type": "uri",
-                "uri": "http://linecorp.com/"
-            }
-        },
-        "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-                {
-                    "type": "text",
-                    "text": "Brown Cafe",
-                    "weight": "bold",
-                    "size": "xl"
-                },
-                {
-                    "type": "box",
-                    "layout": "baseline",
-                    "margin": "md",
-                    "contents": [
-                        {
-                            "type": "icon",
-                            "size": "sm",
-                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                        },
-                        {
-                            "type": "icon",
-                            "size": "sm",
-                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                        },
-                        {
-                            "type": "icon",
-                            "size": "sm",
-                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                        },
-                        {
-                            "type": "icon",
-                            "size": "sm",
-                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                        },
-                        {
-                            "type": "icon",
-                            "size": "sm",
-                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
-                        },
-                        {
-                            "type": "text",
-                            "text": "4.0",
-                            "size": "sm",
-                            "color": "#999999",
-                            "margin": "md",
-                            "flex": 0
-                        }
-                    ]
-                },
-                {
-                    "type": "box",
-                    "layout": "vertical",
-                    "margin": "lg",
-                    "spacing": "sm",
-                    "contents": [
-                        {
-                            "type": "box",
-                            "layout": "baseline",
-                            "spacing": "sm",
-                            "contents": [
-                                {
-                                    "type": "text",
-                                    "text": "Place",
-                                    "color": "#aaaaaa",
-                                    "size": "sm",
-                                    "flex": 1
-                                },
-                                {
-                                    "type": "text",
-                                    "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
-                                    "wrap": True,
-                                    "color": "#666666",
-                                    "size": "sm",
-                                    "flex": 5
-                                }
-                            ]
-                        },
-                        {
-                            "type": "box",
-                            "layout": "baseline",
-                            "spacing": "sm",
-                            "contents": [
-                                {
-                                    "type": "text",
-                                    "text": "Time",
-                                    "color": "#aaaaaa",
-                                    "size": "sm",
-                                    "flex": 1
-                                },
-                                {
-                                    "type": "text",
-                                    "text": "10:00 - 23:00",
-                                    "wrap": True,
-                                    "color": "#666666",
-                                    "size": "sm",
-                                    "flex": 5
-                                }
-                            ]
-                        }
-                    ]
-                }
-            ]
-        },
-        "footer": {
-            "type": "box",
-            "layout": "vertical",
-            "spacing": "sm",
-            "contents": [
-                {
-                    "type": "button",
-                    "style": "link",
-                    "height": "sm",
-                    "action": {
-                        "type": "uri",
-                        "label": "CALL",
-                        "uri": "https://linecorp.com"
-                    }
-                },
-                {
-                    "type": "button",
-                    "style": "link",
-                    "height": "sm",
-                    "action": {
-                        "type": "uri",
-                        "label": "WEBSITE",
-                        "uri": "https://linecorp.com"
-                    }
-                },
-                {
-                    "type": "spacer",
-                    "size": "sm"
-                }
-            ],
-            "flex": 0
+    contents = {
+        'type': 'bubble',
+        'direction': 'ltr',
+        'hero': {
+            'type': 'image',
+            'url': 'https://i.ytimg.com/vi/MwxgUVrj5m4/default.jpg',
+            'size': 'full',
+            'aspectRatio': '20:13',
+            'aspectMode': 'cover',
+            'action': {'type': 'uri', 'uri': 'https://youtu.be/MwxgUVrj5m4', 'label': 'label'}
         }
     }
-    return msg
+    return contents
 
 if __name__ == "__main__":
     #    app.run()
